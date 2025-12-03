568e23c5fb59eede34ac384fde7dbd8d91d84093
im-rk <krramkumar2006g@gmail.com>
2025-12-03 17:12:37 +0530

    Initial commit

---
+++ src/main/java/com/dev/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        SpringApplication.run(BlogApplication.class, args);
+	}
+}
