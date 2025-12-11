09d2bc5d6be22f895f57d3fb592ccc45dc38d793
im-rk <krramkumar2006g@gmail.com>
2025-12-11 18:27:25 +0530

    fixed error in collection(used list instead of set) , and then implemeted controller funciton to the list tags

--- src/main/java/com/dev/blog/mappers/TagMapper.java
+++ src/main/java/com/dev/blog/mappers/TagMapper.java
@@ -11,21 +11,22 @@ import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
+import java.util.Set;
 
-@Mapper(componentModel="spring",unmappedTargetPolicy= ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TagMapper {
-     @Mapping(target="postCount",source="posts",qualifiedByName="calculatePostCount")
+
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
     TagResponse toTagResponse(Tag tag);
 
-     @Named("calculatePostCount")
-    default Integer calculatePostCount(List<Post> posts)
-     {
-         if(posts==null)
-         {
-             return 0;
-         }
-         return (int) posts.stream()
-                 .filter(post-> PostStatus.PUBLISHED.equals(post.getStatus()))
-                 .count();
-     }
+    @Named("calculatePostCount")
+    static Integer calculatePostCount(Set<Post> posts) {
+        if (posts == null) {
+            return 0;
+        }
+        return (int) posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
 }
+
