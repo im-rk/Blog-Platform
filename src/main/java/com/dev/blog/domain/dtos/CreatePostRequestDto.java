a4491f15153bd0035e380c2b81c8f47a8bc278ba
im-rk <krramkumar2006g@gmail.com>
2025-12-16 17:36:47 +0530

    post request dto is implemented

---
+++ src/main/java/com/dev/blog/domain/dtos/CreatePostRequestDto.java
@@ -0,0 +1,32 @@
+package com.dev.blog.domain.dtos;
+
+import com.dev.blog.domain.PostStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class CreatePostRequestDto {
+    @NotBlank(message="Title is required")
+    @Size(min=3,max=200,message="Title must be between {min} and {max} characters")
+    private String title;
+
+    @NotBlank(message = "Not blank content is required")
+    @Size(min=10,max=50000,message = "Content must be between {min} and {max} characters")
+    private String content;
+
+    @NotNull(message = "Category Id is required")
+    private UUID categoryId;
+
+    @Builder.Default
+    @Size(max=10,message = "Maximum {max} tags allowed")
+    private Set<UUID> tagIds=new HashSet<>();
+
+    @NotNull(message = "Status is required")
+    private PostStatus status;
+}
+
